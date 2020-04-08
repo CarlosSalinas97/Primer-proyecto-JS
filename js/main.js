636fecf37ae5bb139f16f4d0fd8f4572bc9b9ed2
CarlosSalinas97 <carlos.ariel.salinas@hotmail.com>
2020-04-08 13:55:19 -0300

    Feature: add post with json

--- js/main.js
+++ js/main.js
@@ -1,8 +1,44 @@
 $(document).ready(function(){
+    //Slider
     $('.bxslider').bxSlider({
         mode: 'fade',
         captions: true,
         slideWidth: 1200,
         responsive: true
     });
+
+    //Posts
+    var posts = [{
+        title: 'Prueba de titulo 1',
+        date: 'Publicado el día: ' + moment().format("D MMMM YYYY"),
+        content: 'Lorem, ipsum 1'
+    },{
+        title: 'Prueba de titulo 2',
+        date: 'Publicado el día: ' + moment().format("D MMMM YYYY"),
+        content: 'Lorem, ipsum 2'
+    },{
+        title: 'Prueba de titulo 3',
+        date: 'Publicado el día: ' + moment().format("D MMMM YYYY"),
+        content: 'Lorem, ipsum 3'
+    },{
+        title: 'Prueba de titulo 4',
+        date: 'Publicado el día: ' + moment().format("D MMMM YYYY"),
+        content: 'Lorem, ipsum 4'
+    },{
+        title: 'Prueba de titulo 5',
+        date: 'Publicado el día: ' + moment().format("D MMMM YYYY"),
+        content: 'Lorem, ipsum 5'
+    }]
+    
+    posts.forEach((item, index)=> {
+        var post = `
+            <article class="post">
+                <h2>${item.title}</h2>
+                <span class="date">${item.date}</span>
+                <p>${item.content}</p>
+                <a href="" class="btn-more">Leer más</a>
+            </article>
+        `
+        $("#posts").append(post);
+    })
 })
