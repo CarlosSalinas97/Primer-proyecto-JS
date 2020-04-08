23d08b294fac4fd17dc86c4d78e707c6af93b5c0
CarlosSalinas97 <carlos.ariel.salinas@hotmail.com>
2020-04-08 14:36:16 -0300

    Feature: fake login

--- js/main.js
+++ js/main.js
@@ -60,6 +60,27 @@ $(document).ready(function(){
         $('html, body').animate({
             scrollTop: 0
         },500)
-        return false
+        return false        //Evita que rediriga la pagina
     })
+
+    //Identificarse mediante JSON (login falso)
+    $('#login form').submit(function(){
+        var form_name = $('#form_name').val();
+        localStorage.setItem("form_name",form_name)
+        location.reload()
+    })
+
+    var form_name = localStorage.getItem("form_name")
+    if(form_name != null && form_name != 'undefined'){
+        var about_parrafo = $('#about p')
+        about_parrafo.html("<br><strong>Bienvenido, "+form_name+"</strong></br>")
+        about_parrafo.append("<a href='#' id='logout'>Cerrar sesión</a>")
+        $('#login').hide()
+
+        $('#logout').click(function(){
+            localStorage.clear()
+            location.reload()
+        })
+    }
+    
 })
